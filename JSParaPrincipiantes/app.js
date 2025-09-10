756ca66b587da8a348f197dce33aeaeebdeadb05
Vannys19 <124051404@upq.edu.mx>
2025-09-09 18:41:18 -0600

    Tipos de datos JavaScript

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,1 +1,29 @@
-document.write("<h1>HELLO WORLD</h1>");
+// Tipos de datos 
+"Hello world" //string
+'Hello world' //string
+
+10000000 // number
+-2.3
+
+//boulean
+true
+false
+
+//array
+['Jose', 'Ryan', 'Martha']
+[1,2,3,4]
+[true,false,true,true]
+
+
+// objet
+'Ryan'
+70.4
+14
+true
+
+{
+    "username": 'Ryan', 
+    "score":70.4, 
+    "hours": 14,
+    "proffesional":true
+}
