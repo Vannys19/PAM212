30d23ae4da2462d954acfe60691756f3589adc80
Vannys19 <124051404@upq.edu.mx>
2025-09-09 18:51:25 -0600

    Tipos de variables

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,29 +1,4 @@
-// Tipos de datos 
-"Hello world" //string
-'Hello world' //string
-
-10000000 // number
--2.3
-
-//boulean
-true
-false
-
-//array
-['Jose', 'Ryan', 'Martha']
-[1,2,3,4]
-[true,false,true,true]
-
-
-// objet
-'Ryan'
-70.4
-14
-true
-
-{
-    "username": 'Ryan', 
-    "score":70.4, 
-    "hours": 14,
-    "proffesional":true
-}
+var nameuser = "JOHN";
+let lastname = "Carter";
+console.log(nameuser)
+console.log(lastname)
