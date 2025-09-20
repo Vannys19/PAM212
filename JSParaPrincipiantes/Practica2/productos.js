d8db5b8a4ce82dd6dc767a0db1d44a4a22d0c670
Vannys19 <124051404@upq.edu.mx>
2025-09-19 19:11:39 -0600

    filter y map en array
    Filtre los datos con filter y cree un nuevo array con map

---
+++ JSParaPrincipiantes/Practica2/productos.js
@@ -0,0 +1,11 @@
+const productos = [
+    {nombre: "Laptop", precio: 12000},
+    {nombre: "Mouse", precio: 250},
+    {nombre: "Teclado", precio: 750},
+    {nombre: "Monitor", precio: 3000},
+];
+let filtrar = productos.filter(producto => producto.precio > 1000);
+console.log(filtrar);
+
+let nombres = filtrar.map(filtra => filtra.nombre);
+console.log(nombres);
